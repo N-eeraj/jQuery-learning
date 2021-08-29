0e885f36ca03d22e07b477f0e3aba9af41d152e6
N-eeraj <neeraj7rajeena@gmail.com>
2021-08-29 11:29:28 +0530

    tried some methods

--- script.js
+++ script.js
@@ -18,9 +18,48 @@ $(document).ready(function() {
         $("#btn2").text("Keep Pressing Me");
     });
 
-    $("#btn3").click(function (e) { 
-        e.preventDefault();
+    $("#btn3").click(function (e) {
         alert("You clicked on the 3rd button");
+    });
+
+    let toggle_var = true;
+    $("#btn4").click(function (e) {
+        if(toggle_var)
+            $("#toggle_text").html("Text has been changed. Click again to toggle back.");
+        else
+            $("#toggle_text").html("Click this button to toggle this text.");
+
+        toggle_var = !toggle_var;
+    });
+
+    $("#btn5").on("click", function () {
+        $("#hide_show").hide();
+    });
+    $("#btn6").on("click", function () {
+        $("#hide_show").show();
+    });
+
+    $("#btn7").hover(function () {
+            // over
+            $("#btn7").css("background-color", "black").css("color", "white");
+        }, function () {
+            // out
+            $("#btn7").css("background-color", "green").css("color", "black");
+        }
+    );
+
+    $("#btn8").on("click", function () {
+        $("#fade").fadeOut();
+    });
+    $("#btn9").on("click", function() {
+        $("#fade").fadeIn();
+    });
+
+    $("#btn10").on("click", function () {
+        alert($("#btn10").prev().html());
+    });
+    $("#btn11").on("click", function () {
+        alert($("#btn11").next().html());
     });
 
 });
